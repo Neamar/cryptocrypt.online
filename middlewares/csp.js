9a46108261eb4f7cc1ea99d427fcebca6b2ad027
Neamar <neamar@neamar.fr>
2024-08-08 14:59:11 +0200

    Add index

--- middlewares/csp.js
+++ middlewares/csp.js
@@ -4,8 +4,10 @@ import { randomUUID } from 'crypto';
 
 
 export const addCSP = async (ctx, next) => {
-  ctx.nonce = randomUUID();
-  ctx.set('Content-Security-Policy', `script-src 'strict-dynamic' 'nonce-${ctx.nonce}' 'unsafe-inline' http: https:; object-src 'none'; base-uri 'none'; style-src 'nonce-${ctx.nonce}'`);
+  if (ctx.request.url !== '/') {
+    ctx.nonce = randomUUID();
+    ctx.set('Content-Security-Policy', `script-src 'strict-dynamic' 'nonce-${ctx.nonce}' 'unsafe-inline' http: https:; object-src 'none'; base-uri 'none'; style-src 'nonce-${ctx.nonce}'`);
+  }
 
   return next();
 };
