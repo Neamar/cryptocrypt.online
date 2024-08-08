65f7297ff8677124481443a598e227304a21095a
Neamar <neamar@neamar.fr>
2024-08-08 16:54:36 +0200

    Add exampl and writing

--- middlewares/csp.js
+++ middlewares/csp.js
@@ -4,7 +4,7 @@ import { randomUUID } from 'crypto';
 
 
 export const addCSP = async (ctx, next) => {
-  if (ctx.request.url !== '/') {
+  if (ctx.request.url !== '/' && ctx.request.url !== '/examples' && ctx.request.url !== '/writing') {
     ctx.nonce = randomUUID();
     ctx.set('Content-Security-Policy', `script-src 'strict-dynamic' 'nonce-${ctx.nonce}' 'unsafe-inline' http: https:; object-src 'none'; base-uri 'none'; style-src 'nonce-${ctx.nonce}'`);
   }
