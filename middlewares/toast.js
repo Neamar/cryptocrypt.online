113ea56d79929dcaece15fc7a0e4164d3c4e0ab1
Neamar <neamar@neamar.fr>
2025-11-01 20:40:18 +0100

    Secure cookies

--- middlewares/toast.js
+++ middlewares/toast.js
@@ -1,3 +1,5 @@
+import { isProd } from '../helpers/env.js';
+
 /**
  * Used as Koa middleware to set the toast directly on the ctx for easy access
  */
@@ -5,9 +7,16 @@ export const readToast = async (ctx, next) => {
   const toast = ctx.cookies.get('toast');
   ctx.toast = toast;
   ctx.toastLevel = ctx.cookies.get('toast-lvl') || 'info';
+
+  const cookieOptions = {
+    httpOnly: true,
+    sameSite: 'lax',
+    ...(isProd && { secure: true }),
+  };
+
   ctx.setToast = (toast, level = 'info') => {
-    ctx.cookies.set('toast', toast);
-    ctx.cookies.set('toast-lvl', level);
+    ctx.cookies.set('toast', toast, cookieOptions);
+    ctx.cookies.set('toast-lvl', level, cookieOptions);
 
     ctx.toast = toast;
     ctx.toastLevel = level;
@@ -17,7 +26,7 @@ export const readToast = async (ctx, next) => {
 
   // Erase toast if page was sent
   if (toast && ctx.status < 300) {
-    ctx.cookies.set('toast');
-    ctx.cookies.set('toast-lvl');
+    ctx.cookies.set('toast', null, cookieOptions);
+    ctx.cookies.set('toast-lvl', null, cookieOptions);
   }
 };
