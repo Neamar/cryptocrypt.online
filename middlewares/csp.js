f0bfd33026ff43cb631f9736c39aae297d600125
Neamar <neamar@neamar.fr>
2025-11-03 12:17:42 +0100

    Use normal template for /examples

--- middlewares/csp.js
+++ middlewares/csp.js
@@ -5,7 +5,7 @@ import { randomUUID } from 'crypto';
 
 export const addCSP = async (ctx, next) => {
   // TODO: after migrating away from webflow, remove this
-  if (ctx.request.url !== '/' && ctx.request.url !== '/examples') {
+  if (ctx.request.url !== '/') {
     ctx.nonce = randomUUID();
     ctx.set('Content-Security-Policy', `script-src 'strict-dynamic' 'nonce-${ctx.nonce}'; object-src 'none'; base-uri 'none'; style-src 'nonce-${ctx.nonce}'`);
   }
